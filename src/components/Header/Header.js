5035c0a4bbb322515382bb183ef81048b7a79b77
annvodoley <roxy-222@mail.ru>
2022-06-28 23:03:36 +0300

    create Header, Header_logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import classes from './Header.module.scss'
+import icon from '../../images/yakitoria_logo.svg'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className={classes.Header_main}>
+            <div className={classes.Header_main__container}>
+                <div className={classes.Header_logo}>
+                    <img src={icon} className={classes.Header_logo__icon}/>
+                </div>
+                <nav className={classes.Navigation}>
+                    <ul className={classes.Navigation_menu}>
+                        <li className={classes.Navigation_menu__item}>
+                            <a href="/news/" className={classes.Navigation_menu__links}>Акции</a>
+                        </li>
+                        <li className={classes.Navigation_menu__item}>
+                            <a href="/restaurants/" className={classes.Navigation_menu__links}>Рестораны</a>
+                        </li>
+                        <li className={classes.Navigation_menu__item}>
+                            <a href="/delivery/" className={classes.Navigation_menu__links}>О доставке</a>
+                        </li>
+                        <li className={classes.Navigation_menu__item}>
+                            <a href="/yakitoria/" className={classes.Navigation_menu__links}>Якитория помогает</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 };
