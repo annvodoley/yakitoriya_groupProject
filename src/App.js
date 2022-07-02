60c7b0c3f93074dd13030ebdb123bc59d1b137df
javan7 <orucovcavanshir@gmail.com>
2022-07-02 03:10:30 +0300

    menu item add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.module.scss";
 import Cart from "./components/Cart/Cart";
+import MenuItem from "./components/MenuItem/MenuItem";
+import classes from "../src/components/MenuItem/MenuItem.module.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Cart />
+      <div className={classes.catalog}>
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+      </div>
     </div>
   );
 }
