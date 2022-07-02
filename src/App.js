20c8f178d4737ba5db7e8ed12b0f69b256d12807
Danila Kuznetsov <danila.kuznetsov@onlinesup.com>
2022-07-02 10:29:41 +0300

    ready SectionMenu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Routes } from 'react-router-dom';
+import {Routes} from 'react-router-dom';
 import './App.module.scss';
 import SectionMenu from './components/SectionMenu/SectionMenu';
 
 
 function App() {
-  return (
-    <div className="App">
-<SectionMenu/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <SectionMenu text='я люблю метро' imgUrl='https://firebasestorage.googleapis.com/v0/b/tabs-e2a0a.appspot.com/o/sasimi.png?alt=media&token=3deb73ab-9f35-4ac9-bab8-3f66f82c8f63'/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/SectionMenu/SectionMenu.js
+++ src/components/SectionMenu/SectionMenu.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import classes from "./SectionMenu.scss";
-import icon from "./images/metro.png";
+import classes from "./SectionMenu.module.scss";
 
-const SectionMenu = () => {
+const SectionMenu = ({imgUrl, text}) => {
     return(
-        <div>
-         <nav className={classes.SectionMenu}>
-             <ul className={classes.menu_List}>
-                <li className={classes.menu_List_Item}>
-                    <NavLink to="/">Я люблю Метро</NavLink>
-                        <img src={icon} className={classes.menu_List_Pic}></img>
-                </li>
-             </ul>
-         </nav>
+        <div className={classes.SectionMenu}>
+            <img src={imgUrl} alt=""/>
+            <p>{text}</p>
          </div>
     );
 };
