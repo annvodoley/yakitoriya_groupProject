2c93bf13b62e8c1a89070b24620ef9c19f95919a
javan7 <orucovcavanshir@gmail.com>
2022-07-05 09:15:51 +0300

    a

--- src/state/yakitoriya_state/actions.js
+++ src/state/yakitoriya_state/actions.js
@@ -1,7 +1,12 @@
-import {ADD_MENU_LIST, ADD_ROLLS, ADD_SALATS, ADD_SETS, ADD_SOUPS} from "./types";
+import {
+  ADD_MENU_LIST,
+  ADD_ROLLS,
+  ADD_SALATS,
+  ADD_SETS,
+  ADD_SOUPS,
+} from "./types";
 import { ADD_SASIMI } from "./types";
-import { ADD_HOT_ROLLS} from "./types";
-
+import { ADD_HOT_ROLLS } from "./types";
 
 //request MenuListAction
 export const fetchMenuListAction = () => {
@@ -22,42 +27,41 @@ export const fetchSasimiAction = () => {
 //request hotrolls
 export const fetchHotRollAction = () => {
   return (dispatch) =>
-      fetch("https://roll-store.herokuapp.com/menu/hot-rolls")
-          .then((res) => res.json())
-          .then((res) => dispatch(addHotRollAction(res)));
+    fetch("https://roll-store.herokuapp.com/menu/hot-rolls")
+      .then((res) => res.json())
+      .then((res) => dispatch(addHotRollAction(res)));
 };
-
 
 //request salats
 export const fetchSalatsAction = () => {
   return (dispatch) =>
-      fetch("https://roll-store.herokuapp.com/menu/salats")
-          .then((res) => res.json())
-          .then((res) => dispatch(addSalatsAction(res)));
+    fetch("https://roll-store.herokuapp.com/menu/salats")
+      .then((res) => res.json())
+      .then((res) => dispatch(addSalatsAction(res)));
 };
 
 //request sets
 export const fetchSetsAction = () => {
   return (dispatch) =>
-      fetch("https://roll-store.herokuapp.com/menu/sets")
-          .then((res) => res.json())
-          .then((res) => dispatch(addSetsAction(res)));
+    fetch("https://roll-store.herokuapp.com/menu/sets")
+      .then((res) => res.json())
+      .then((res) => dispatch(addSetsAction(res)));
 };
 
 //request soups
 export const fetchSoupsAction = () => {
   return (dispatch) =>
-      fetch("https://roll-store.herokuapp.com/menu/soups")
-          .then((res) => res.json())
-          .then((res) => dispatch(addSoupsAction(res)));
+    fetch("https://roll-store.herokuapp.com/menu/soups")
+      .then((res) => res.json())
+      .then((res) => dispatch(addSoupsAction(res)));
 };
 
 //request rolls
 export const fetchRollsAction = () => {
   return (dispatch) =>
-      fetch("https://roll-store.herokuapp.com/menu/rolls")
-          .then((res) => res.json())
-          .then((res) => dispatch(addSRollsAction(res)));
+    fetch("https://roll-store.herokuapp.com/menu/rolls")
+      .then((res) => res.json())
+      .then((res) => dispatch(addSRollsAction(res)));
 };
 
 //menuListAction
@@ -66,20 +70,17 @@ export const addMenuListAction = (menuList) => ({
   payload: menuList,
 });
 
-
 //sasimiAction
 export const addSasimiAction = (sasimi) => ({
   type: ADD_SASIMI,
   payload: sasimi,
 });
 
-
 //hotrollsAction
 export const addHotRollAction = (hotrolls) => ({
   type: ADD_HOT_ROLLS,
   payload: hotrolls,
 });
-
 
 //salatsAction
 export const addSalatsAction = (salats) => ({
